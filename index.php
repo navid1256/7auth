80f659511565c6e30054fb67102d4598a9afec8a
navid1256 <navid.syndicate@gmail.com>
2026-05-18 00:20:14 +0330

    make Project Directory and Folders

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+</head>
+<body>
+    <h1>Welcome to your profile page!</h1>
+    <p>This is a protected area. Only authenticated users can access this page.</p>
+    
+</body>
+</html>
--- views/register-view.php
+++ views/register-view.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Register Page</title>
-    <link rel="stylesheet" type="text/css" href="./styles.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/styles.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.2.0/mdb.min.css" rel="stylesheet" />
@@ -50,7 +50,8 @@
 
                                 <!-- Checkbox -->
                                 <div class="form-check d-flex justify-content-center mb-4">
-                                    <input class="form-check-input me-2" type="checkbox" value="" id="remember-me" checked />
+                                    <input class="form-check-input me-2" type="checkbox" value="" id="remember-me"
+                                        checked />
                                     <label class="form-check-label" for="remember-me">
                                         Remember Me
                                     </label>
